4d1c7467aa493294eb4f84e2b714075703f714a0
gpablo3 <giancarlo@api3.org>
2022-10-25 22:30:07 -0600

    Add casting utility for Decimals

--- pyliquid/utils/data.py
+++ pyliquid/utils/data.py
@@ -3,9 +3,11 @@ Utilities for data manipulation and validation
 """
 
 import logging
+from typing import Optional
+from decimal import Decimal
 
 
-def check_sorted_index_tuple(input_list: list[tuple]) -> bool:
+def check_sorted_index_tuple(input_list: list[tuple]) -> Optional[bool]:
     """
     Verify if the input list is already sorted based on sub-indexes from
     tuple.
@@ -26,3 +28,28 @@ def check_sorted_index_tuple(input_list: list[tuple]) -> bool:
     except TypeError:
         logging.error('It was not posisble to sort the list. \
             Review the given tuples.')
+
+def parse_decimal_to_float(input_dict: dict) -> dict:
+    """
+    Cast any decimal type to floats or integer. 
+    This enables `json` package to dumps responses from rpc response.
+
+    Paramaters
+    ----------
+    input_dict: dict
+        The raw data to be casted and formatted.
+
+    Returns
+    -------
+    dict
+        The resulting dictionary with compatible types.
+    """
+    to_return = {}
+    for k, v in input_dict.items():
+        if type(v) == Decimal:
+            to_return[k] = float(v)
+        elif type(v) == dict:
+            to_return.update({k: parse_decimal_to_float(v)})
+        else:
+            to_return[k] = v
+    return to_return
